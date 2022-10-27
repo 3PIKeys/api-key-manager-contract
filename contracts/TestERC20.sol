3e93c55f020396a5f35bf6b9e7f2064269feeeb9
trmid <trmidpoint@gmail.com>
2022-10-27 14:54:27 -0400

    increase deployment mint

--- contracts/TestERC20.sol
+++ contracts/TestERC20.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestERC20 is ERC20 {
   constructor() ERC20("Gimme Token", "GIM") {
-    _mint(_msgSender(), 1000000 * (10**18));
+    _mint(_msgSender(), 1000000000 * (10**18));
   }
 }
